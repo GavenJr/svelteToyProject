e435dbab3ef8342f10b653f21ada9e83fb41da55
GavenJr <61298885+GavenJr@users.noreply.github.com>
2023-10-22 22:06:45 -0300

    aaaaaa

---
+++ src/app.svelte
@@ -0,0 +1,16 @@
+<!--Svelte es una herramienta para construir apps web
+Permite construir una app declarativamente desde modulos, 
+combinando marcado, estilos y comportamientos.
+Estos componentes son compilados en pequeños modulos de JS
+los cuales eliminan la saturacion que tendria un framework de UI-->
+
+<script>
+    // your script goes here
+    let name = 'Svelte';
+    let src = "/image.png"
+    let imgDescr = "GavenJr"
+</script>>
+
+<h1>Mi app en { name.toLowerCase() }!</h1>
+
+<img src={src} alt="Una imagen de {imgDescr}"/>
